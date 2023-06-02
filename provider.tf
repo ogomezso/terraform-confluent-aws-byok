ab262eaf0ee8d5999733316d425968eedd5a2f5a
Oscar Gomez Soriano <ogomezso0@gmail.com>
2023-06-02 16:20:52 +0100

    adding confluent provider

--- provider.tf
+++ provider.tf
@@ -15,3 +15,9 @@ terraform {
 provider "aws" {
   region     = var.aws_region
 }
+
+provider "confluent" {  
+  alias = "confluent_cloud"
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
